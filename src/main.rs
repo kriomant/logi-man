41235274a4986880442db8b32fc9cf081661d0d7
Mikhail Trishchenkov <kriomant@yandex-team.ru>
2025-04-07 23:43:14 +0700

    Add command to list devices

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{collections::BTreeMap, path::Path};
 
 use clap::{Parser, Subcommand};
 use eyre::{ensure, Result};
@@ -17,11 +17,56 @@ struct Options {
 #[derive(Subcommand)]
 enum Command {
     ShowSettings,
+    ListDevices,
 }
 
 #[derive(Deserialize, Serialize)]
 struct Settings {
     profile_keys: Vec<String>,
+    ever_connected_devices: EverConnectedDevices,
+    migration_report: MigrationReport,
+
+    #[serde(flatten)]
+    rest: Map<String, Value>,
+}
+
+#[derive(Deserialize, Serialize)]
+struct EverConnectedDevices {
+    devices: Vec<ConnectedDevice>,
+
+    #[serde(flatten)]
+    rest: Map<String, Value>,
+}
+
+#[derive(Deserialize, Serialize)]
+struct ConnectedDevice {
+    #[serde(rename="connectionType")]
+    connection_type: String,
+    #[serde(rename="deviceModel")]
+    device_model: String,
+    #[serde(rename="deviceType")]
+    device_type: String,
+    #[serde(rename="slotPrefix")]
+    slot_prefix: String,
+
+    #[serde(flatten)]
+    rest: Map<String, Value>,
+}
+
+#[derive(Deserialize, Serialize)]
+struct MigrationReport {
+    devices: Vec<MigrationDevice>,
+
+    #[serde(flatten)]
+    rest: Map<String, Value>,
+}
+
+#[derive(Deserialize, Serialize)]
+struct MigrationDevice {
+    #[serde(rename="deviceName")]
+    device_name: String,
+    #[serde(rename="modelId")]
+    model_id: String,
 
     #[serde(flatten)]
     rest: Map<String, Value>,
@@ -52,6 +97,35 @@ fn main() -> Result<()> {
 
     match options.command {
         Command::ShowSettings => serde_json::to_writer_pretty(std::io::stdout(), &settings)?,
+        Command::ListDevices => {
+            // Get human-readable model names. I have no idea where LogiOptions application
+            // gets them, I suppose they are hardcoded into binary. But some model names
+            // are in migration settings. Load them and use.
+            let model_names: BTreeMap<&str, &str> = settings.migration_report.devices.iter()
+                .map(|device| (device.model_id.as_str(), device.device_name.as_str()))
+                .collect();
+
+            let devices: BTreeMap<&str, &ConnectedDevice> = settings.ever_connected_devices.devices.iter()
+                // There are some virtual devices in list, skip them.
+                .filter(|device| device.device_type == "MOUSE")
+                // Sometimes same device is listed several times. Deduplicate records.
+                .map(|device| (device.slot_prefix.as_str(), device))
+                .collect();
+
+            for device in devices.values() {
+                let model_name: &str = model_names.get(device.device_model.as_str()).cloned()
+                    // Sometimes model ID in migration settings looks like '6b023',
+                    // but device model in device list is '6b023_ext2'.
+                    // So try to use first part before '_' to find model name.
+                    .or_else(|| {
+                        device.device_model.split_once('_')
+                            .and_then(|(prefix, _)| model_names.get(prefix).cloned())
+                    })
+                    // No model name found, use model id.
+                    .unwrap_or(device.device_model.as_str());
+                println!("{}: {}", device.slot_prefix, model_name);
+            }
+        }
     }
 
     Ok(())
